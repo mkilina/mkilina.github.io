9ac2cd575218b6b5fab3854470e25239bf113ad9
lauberto <55099845+lauberto@users.noreply.github.com>
2020-12-15 22:22:32 +0300

    new database search function

--- route.py
+++ route.py
@@ -1,15 +1,43 @@
 # -*- coding: utf-8 -*- 
 from flask import *
+from flask_mysqldb import MySQL
+import json
 #from app import app
 
 app = Flask(__name__)
+mysql = MySQL(app)
+
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'andrea'
+app.config['MYSQL_PASSWORD'] = 'rstq!2Ro'
+app.config['MYSQL_DB'] = 'cat'
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/search')
+@app.route('/search',  methods=['GET', 'POST'])
 def search():
-    return render_template('search.html', title='Search')
+    if request.method == "GET":
+        return render_template('search.html', title='Search')
+
+    else:
+        details = request.form
+        search_token = details['search']
+        print(search_token)
+        cur = mysql.connection.cursor()
+        cur.execute(f'''SELECT "freq_all", freq_all, "freq1", freq1 
+        from unigrams WHERE unigram = "{search_token}";''')
+        row_headers = [x[0] for x in cur.description]
+        rv = cur.fetchall()
+        json_data = []
+        for result in rv:
+            json_data.append(dict(zip(row_headers, result)))
+        return render_template('db_response.html', response=json.dumps(json_data), token=search_token)
 
 @app.route('/search_morph')
 def search_morph():
@@ -36,4 +64,4 @@ def main():
     return render_template('main.html', title='About us')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
